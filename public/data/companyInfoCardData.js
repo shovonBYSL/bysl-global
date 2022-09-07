10249a2de6409785dd5e0698900a045c0f515807
shovonBYSL <kawser.shovon@intelli.global>
2022-09-07 16:43:49 +0600

    updated about us

--- public/data/companyInfoCardData.js
+++ public/data/companyInfoCardData.js
@@ -27,7 +27,7 @@ const purposeCardData = [
     id: 1,
     title: "Continuous Investment",
     description:
-      "Investment at BYSL Global follows the steps of exploration and innovation. The validation and refinement of ideas and innovation in any field must pass a certain bench mark before getting selected for capital investment. We only make long-term investments where we can create an impact at scale. What differentiates BYSL Global from any other business organization is its purpose of investments. The aim of our investment goes beyond profit-making. Our investments are guided by our vision to establish a better future for everyone. We invest in meaningful ventures through which we can make socio-economic contributions.",
+      "Investment at BYSL Global follows the steps of exploration and innovation. The validation and refinement of ideas and innovation in any field must pass a certain bench mark before getting selected for capital investment. We only make long-term investments where we can create an impact at scale. What differentiates BYSL Global from any other business organization is its purpose of investments. The aim of our investment goes beyond profit-ma king. Our investments are guided by our vision to establish a better future for everyone. We invest in meaningful ventures through which we can make socio-economic contributions.",
     img: "purpose_card_1",
   },
   {
@@ -53,25 +53,26 @@ const valuesCardData = [
     id: 0,
     title: "Principles & Beliefs",
     description:
-      "BYSL Global's principles - honesty, integrity, and permissibility - are derived from Islamic principles. Honesty dictates individuals to be true to not only others but also to themselves. Integrity goes even further than that; it implies the purity of soul and intentions achieved through strong moral and ethical beliefs. The strength of faith, trust, and strong belief in the organization is expected to be emitted through the powerful character and noble manners of BYSL Global's employees. ",
+      "BYSL Global's principles - honesty, integrity, and permissibility - are derived from Islamic principles. Honesty dictates individuals to be true to not only others but also to themselves. Integrity goes even further than that; it implies the purity of soul and intentions achieved through strong moral and ethical beliefs. The strength of faith, trust, and strong belief in the organization is expected to be emitted through the powerful character and noble manners of BYSL Global's employees.",
     description1:
-      "Furthermore, the belief system based on the purpose, mission, vision, and core values of the organization, in its success and prosperity in the long term, should act as a constant source of motivation, confidence, and a driving force for the employees. ",
+      "Furthermore, the belief system based on the purpose, mission, vision, and core values of the organization, in its success and prosperity in the long term, should act as a constant source of motivation, confidence, and a driving force for the employees.",
     description2:
       "All stakeholders and employees are expected to commit to these ideological constants with the highest level of sincerity and belief.",
     img: "values_card_0",
   },
+
   {
     id: 1,
     title: "Customer Experience & Satisfaction",
     description:
-      "Enhancing customer satisfaction remains central to BYSL Global's business approaches. Customer satisfaction is a constant effort leading to a never-ending pursuit of exceeding customers’ expectations. To enrich customer experience, the organization takes special care in improving every mode of communication and interaction with the customers. Our commitment to customers is reflected through providing them with exceptional products and services, and creating unparalleled impressions at all levels of their journey. ",
+      "Enhancing customer satisfaction remains central to BYSL Global's business approaches. Customer satisfaction is a constant effort leading to a never-ending pursuit of exceeding customers’ expectations. To enrich customer experience, the organization takes special care in improving every mode of communication and interaction with the customers. Our commitment to customers is reflected through providing them with exceptional products and services, and creating unparalleled impressions at all levels of their journey.",
     img: "values_card_1",
   },
   {
     id: 2,
     title: "Exceptionalism & High Standards",
     description:
-      "BYSL Global adopts the term 'Exceptionalism' to mean being different from the norm, what is common and ordinary, in a superior way. This concept is finely in tune with the organization's mission to serve the world with exceptional products and services. We promote exceptionalism in every aspect of our business institution and maintain high standards in everything we do. The standard of something is relative, and high standards are ensured in relation to the particular industry, market, product, or service.  ",
+      "BYSL Global adopts the term 'Exceptionalism' to mean being different from the norm, what is common and ordinary, in a superior way. This concept is finely in tune with the organization's mission to serve the world with exceptional products and services. We promote exceptionalism in every aspect of our business institution and maintain high standards in everything we do. The standard of something is relative, and high standards are ensured in relation to the particular industry, market, product, or service.",
     description1:
       "In other words, whatever we do, we must do it differently and better than our peers or competitors.",
     img: "values_card_2",
@@ -80,9 +81,9 @@ const valuesCardData = [
     id: 3,
     title: "Employee Wellbeing & Team Spirit",
     description:
-      "Employee well-being has been one of the key focuses of BYSL Global. It is more than ensuring financial and career growth of the employees, the organization cares about the employees’ spiritual and psychological upliftment as well. A state-of-the-art working environment is ensured to inspire creativity and innovation. Strong team support and fairness of administrative and management policies fulfill the fundamental requirements for employee well-being.     ",
+      "Employee well-being has been one of the key focuses of BYSL Global. It is more than ensuring financial and career growth of the employees, the organization cares about the employees’ spiritual and psychological upliftment as well. A state-of-the-art working environment is ensured to inspire creativity and innovation. Strong team support and fairness of administrative and management policies fulfill the fundamental requirements for employee well-being.",
     description1:
-      "Team spirit springs from how efficiently, responsibly, and respectfully the team members treat, hear and cooperate with one another. The organization focuses on building a psychologically safe atmosphere for enhancing team spirit so that teams can innovate faster, achieve higher productivity, manage risks and find better solutions to problems. ",
+      "Team spirit springs from how efficiently, responsibly, and respectfully the team members treat, hear and cooperate with one another. The organization focuses on building a psychologically safe atmosphere for enhancing team spirit so that teams can innovate faster, achieve higher productivity, manage risks and find better solutions to problems.",
     img: "values_card_3",
   },
 ];
@@ -90,16 +91,16 @@ const valuesCardData = [
 const cultureCardData = [
   {
     id: 0,
-    title: "Forward-thinking",
+    title: "Forward-Thinking",
     description:
-      "Forward-thinking is inextricably  linked to innovation and progress. It is an approach to  fuel  innovation,  nurture  the  right conditions for new ideas, and explore the realm  of  possibilities.  Forward-thinking enables us to envision futuristic solutions to past  and  present  problems  and  move forward with profound optimism even in the face  of  challenges.  In  times  of  crises, desperation,  and  uncertainties,  forward- thinking automatically inspires and activates optimism to sail through obstacles when the vision is blurred. Because of these factors, BYSL Global encourages all employees to internalize and adopt forward-thinking to solve problems by finding dynamic and innovative solutions.",
+      "Forward-thinking is inextricably linked to innovation and progress. It is an approach to fuel innovation, nurture the right conditions for new ideas, and explore the realm of possibilities. Forward-thinking enables us to envision futuristic solutions to past and present problems and move forward with profound optimism even in the face of challenges. In times of crises, desperation, and uncertainties, forward-thinking automatically inspires and activates optimism to sail through obstacles when the vision is blurred. Because of these factors, BYSL Global encourages all employees to internalize and adopt forward-thinking to solve problems by finding dynamic and innovative solutions.",
     img: "culture_card_0",
   },
   {
     id: 1,
     title: "Learning",
     description:
-      "BYSL Global promotes  and nurtures a culture  of  learning  that  is  structured, validated, and productive. More precisely, the promoted learning process should go through a continuous experimentation and feedback cycle based on data and metrics. Learning at the premise of BYSL Global aims and carries the potential to spur incremental and disruptive innovation in all relevant fields. Apart from structured and validated learning, employees are encouraged to keep an open mind to learn from mistkes and explore new solutions to problems. For steady growth and development of the organization, learning plays a vital role, and every employee is expected to be an active learner.",
+      "BYSL Global promotes and nurtures a culture of learning that is structured, validated, and productive. More precisely, the promoted learning process should go through a continuous experimentation and feedback cycle based on data and metrics. Learning at the premise of BYSL Global aims and carries the potential to spur incremental and disruptive innovation in all relevant fields. Apart from structured and validated learning, employees are encouraged to keep an open mind to learn from mistakes and explore new solutions to problems. For steady growth and development of the organization, learning plays a vital role, and every employee is expected to be an active learner.",
     img: "culture_card_1",
   },
   {
@@ -111,32 +112,33 @@ const cultureCardData = [
       "The development process at BYSL Global goes through methodical exploration and innovation phases, adapting the 'Lean' development methodology for exploration of ideas and the 'Agile' development methodology for innovation. These development methodologies help ensure better results by creating adaptive strategies that are flexible enough to change directions, optimize, increase collaboration, create feedback cycles, minimize working processes, eliminate waste, and deliver the maximum value. BYSL Global makes it mandatory for all employees to cultivate a development mindset.",
     img: "culture_card_2",
   },
+
   {
     id: 3,
     title: "Change",
     description:
-      "BYSL  Global  operates  as  a  dynamic organization that is constantly evolving and upgrading with time. Change is a part of our cultural  fabric  that inspires  dynamism  in everything we do. We embrace change with full-blown  optimism,  adapt  to  it  in  a onstructive  way  to  turn  obstacles  into opportunities,  and  maintain  a  steady momentum towards our vision. Essentially, change remains the underlying force that adds  fluidity  to  all  our  operations.  It constitutes  a  culture  of  adaptation,resilience, and innovation. Owing to change, the dynamism in every aspect of theorganization makes it even more resilient to adversaries while adding agility to explore and utilize new opportunities.",
+      "BYSL Global operates as a dynamic organization that is constantly evolving and upgrading with time. Change is a part of our cultural fabric that inspires dynamism in everything we do. We embrace change with full-blown optimism, adapt to it in a constructive way to turn obstacles into opportunities, and maintain a steady momentum towards our vision. Essentially, change remains the underlying force that adds fluidity to all our operations. It constitutes a culture of adaptation, resilience, and innovation. Owing to change, the dynamism in every aspect of the organization makes it even more resilient to adversaries while adding agility to explore and utilize new opportunities.",
     img: "culture_card_3",
   },
   {
     id: 4,
     title: "Teamwork",
     description:
-      "BYSL  Global  promotes  a  culture  of teamwork  on its premises. No ’one-man army’ is encouraged to emerge; instead, teams are modeled and empowered to be the  fundamental  working  units.  The organization  promotes  that  goals  are achieved through effective teamwork. Every success  or  achievement  is  mainly  the success or achievement of the team. Here, team accomplishments are orioritized over individual achievements.",
+      "BYSL Global promotes a culture of teamwork on its premises. No 'one-man army' is encouraged to emerge; instead, teams are modeled and empowered to be the fundamental working units. The organization promotes that goals are achieved through effective teamwork. Every success or achievement is mainly the success or achievement of the team. Here, team accomplishments are prioritized over individual achievements.",
     img: "culture_card_4",
   },
   {
     id: 5,
     title: "Innovation",
     description:
-      "Rising from the ambition to serve the world with  exceptional  products  and  services, BYSL Global  has weaved  the culture  of innovation  at  the  very  heart  of  the organization. Inspiring continuous explora- tions through systematic experimentations, our culture fuels innovation in a way that ushers a stream of new ideas and concepts for  exceptional  products  and  services. Exceptional  products  and  services  are empowering, and they excel in creativity and  productivity;  they  work  as  an enhancement of life. We aspire to develop innovative products and services to touch  and  change  people's  lives  and  add  quality  to  them.  We  aspire  to revolutionize the life and work of our valued customers by ascending to the next curve of innovation, unfolding the hidden blessings of life that we all are subjected to. Everyone belonging to the organization devotes their full potential to keep the engine of innovation revving.",
+      "Rising from the ambition to serve the world with exceptional products and services, BYSL Global has weaved the culture of innovation at the very heart of the organization. Inspiring continuous explorations through systematic experimentations, our culture fuels innovation in a way that ushers a stream of new ideas and concepts for exceptional products and services. Exceptional products and services are empowering, and they excel in creativity and productivity; they work as an enhancement of life. We aspire to develop innovative products and services to touch and change people's lives and add quality to them. We aspire to revolutionize the life and work of our valued customers by ascending to the next curve of innovation, unfolding the hidden blessings of life that we all are subjected to. Everyone belonging to the organization devotes their full potential to keep the engine of innovation revving.",
     img: "culture_card_5",
   },
   {
     id: 6,
     title: "Integrity",
     description:
-      "Integrity is a higher form of honesty guided by moral principles and an indestructible commitment to those principles. It is one of the  core  principles  promoted  by  BYSL Global and is expected to be acquired by all employees.  The  organization  separates individuals   into   ’acceptable’ and 'unacceptable’  based  on  integrity.  Even someone who is the best in the industry in terms of skill, knowledge, and expertise may be  unacceptable  without  having  this superior  human quality.  Honesty  and morality are valued more than any other human qualities on the premise of BYSL Global.",
+      "Integrity is a higher form of honesty guided by moral principles and an indestructible commitment to those principles. It is one of the core principles promoted by BYSL Global and is expected to be acquired by all employees. The organization separates individuals into 'acceptable' and 'unacceptable' based on integrity. Even someone who is the best in the industry in terms of skill, knowledge, and expertise may be unacceptable without having this superior human quality. Honesty and morality are valued more than any other human qualities on the premise of BYSL Global.",
     img: "culture_card_6",
   },
 ];
