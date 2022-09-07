16895cb477ee9438afa24bee7708ad2f75b66ef5
shovonBYSL <kawser.shovon@intelli.global>
2022-09-07 18:58:13 +0600

    job description fixed

--- pages/careers/job-description/[jobId].js
+++ pages/careers/job-description/[jobId].js
@@ -29,7 +29,14 @@ const JobDescription = () => {
         <CommonLayout title="Job Description">
           <div className="max-w-[856px] mx-auto py-10 xl:py-16">
             <div className="lg:flex justify-between items-center mb-6">
-              <JobTitle title={data.position} />
+              <div className="text-center lg:text-start">
+                <p className="text-gray-800 text-2xl font-semibold">
+                  {data.position}
+                </p>
+                <p className="mt-4 mb-10 lg:mb-0 text-gray-600 text-lg font-medium">
+                  BYSL Global Technology Group
+                </p>
+              </div>
               <Link passHref href={`/careers/application-form/${data.id}`}>
                 <div className="w-max h-max mx-auto lg:mx-0 text-center rounded-lg text-white text-sm font-semibold px-8 xl:px-[42px] py-2 xl:py-2.5 hover:cursor-pointer bg-gradient-to-r hover:from-blue-600 hover:to-blue-800  from-blue-900 to-blue-700">
                   Apply Now
